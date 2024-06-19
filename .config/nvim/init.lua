a0a41d75923cb3b947defe79edb879805d8337f7
Tiemo Kieft <github@isogram.nl>
2024-06-19 05:57:41 +0200

    Add yabai and skhd

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -513,16 +513,17 @@ require('lazy').setup({
   },
 
   {
-    'folke/tokyonight.nvim',
+    -- 'folke/tokyonight.nvim',
+    'sainnhe/sonokai',
     priority = 1000, -- Make sure to load this before all the other start plugins.
     init = function()
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
       -- vim.cmd.colorscheme 'tokyonight-night'
+      vim.cmd.colorscheme 'sonokai'
 
       -- You can configure highlights by doing something like:
-      vim.cmd.hi 'Comment gui=none'
     end,
   },
 
